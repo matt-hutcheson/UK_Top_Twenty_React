16d95a1158406fae111ca23fb1e9ce6d8ace579c
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-12-08 16:29:05 +0000

    added hitdetails component

---
+++ src/components/HitDetails.js
@@ -0,0 +1,7 @@
+const HitDetails = () => {
+  return (
+    <h2>Hit Details</h2>
+  )
+}
+
+export default HitDetails;
--- src/components/HitsList.js
+++ src/components/HitsList.js
@@ -1,6 +1,12 @@
+import HitDetails from './HitDetails';
+
 const HitsList = () => {
   return (
-    <h1>HitsList</h1>
+    <>
+      <h1>HitsList</h1>
+      <HitDetails />
+    </>
+
   )
 }
 
